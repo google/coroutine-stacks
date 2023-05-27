203f1523120e95a4633ace47f4d47fc640582105
Nikita Nazarov <nikitanazarov@google.com>
2023-05-27 18:05:39 +0200

    Change intellij version and add Kotlin plugin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,10 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.2.5")
+    version.set("2023.1.2")
     type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+    plugins.set(listOf("org.jetbrains.kotlin"))
 }
 
 tasks {
