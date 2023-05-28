648f08c1fa342ef2ee558c834492eb53cbef68ba
Nikita Nazarov <nikitanazarov@google.com>
2023-05-29 01:56:20 +0200

    Add CoroutineStacksBundle

---
+++ src/main/kotlin/com/nikitanazarov/coroutinestacks/CoroutineStacksBundle.kt
@@ -0,0 +1,15 @@
+package com.nikitanazarov.coroutinestacks
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+
+@NonNls
+private const val BUNDLE = "messages.CoroutineStacksBundle"
+
+object CoroutineStacksBundle : DynamicBundle(BUNDLE) {
+    @Nls
+    @JvmStatic
+    fun message(@NonNls @PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
+}
