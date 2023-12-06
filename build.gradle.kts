3d97372fd7f41c318bb9ad147fcd5fed6f238552
Nikita Nazarov <nikitanazarov@google.com>
2023-12-06 13:47:28 +0100

    Increase until-version to make the plugin available for EAP builds
    Closes #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.google"
-version = "1.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.2.5")
+    version.set("2023.3")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf("org.jetbrains.kotlin"))
@@ -28,11 +28,6 @@ tasks {
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("222")
-        untilBuild.set("232.*")
     }
 
     signPlugin {
